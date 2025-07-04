c7f09ea4c9fd7bf72ea64f84605128e0a5890988
Calcifer-02 <02.calcifer@gmail.com>
2025-07-04 16:46:05 +0300

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-   base: process.env.NODE_ENV === "production" ? "/my-recruto-app/" : "/",
+   plugins: [react()],
+   base: "/my-recruto-app/", // <-- важно!
 });
